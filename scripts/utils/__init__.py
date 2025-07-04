7d9bca9b8f9a0f1d9389072654cebfaed1fac02d
fabianVagnoni <153111960+fabianVagnoni@users.noreply.github.com>
2025-07-04 09:42:55 +0200

    Small Package Correction

--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -3,4 +3,4 @@
 # Utils package for SCOTUS AI pipeline 
 
 # Import commonly used functions
-from .metrics import calculate_f1_macro, calculate_comprehensive_metrics, calculate_confusion_matrix_metrics
+from .metrics import calculate_f1_macro
